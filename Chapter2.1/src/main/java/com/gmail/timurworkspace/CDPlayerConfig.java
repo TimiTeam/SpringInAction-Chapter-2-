68674c4f7b59f3268b489cace88742dbb25e5314
Timur <timatiblackstar666@gmail.com>
2018-05-04 16:39:47 +0300

    Declaring components in JavaConfig

--- Chapter2.1/src/main/java/com/gmail/timurworkspace/CDPlayerConfig.java
+++ Chapter2.1/src/main/java/com/gmail/timurworkspace/CDPlayerConfig.java
@@ -9,10 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // annotation identifies this as a configuration class,
 // and it’s expected to contain details on beans that
 // are to be created in theSpring application context.
-@ComponentScan(basePackageClasses = SgtPeppers.class) // Will be scan that package
+//@ComponentScan(basePackageClasses = SgtPeppers.class) // Will be scan that package
 // and any subpackages underneath it,looking for classes that are annotated with @Component.
 // It should find the Compact-Disc class and automatically create a bean for it in Spring.
 public class CDPlayerConfig {
 
-
+    @Bean
+    public CompactDisc sgtPeppers(){
+        return new SgtPeppers();
+    }
 }
