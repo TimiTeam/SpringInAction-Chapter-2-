8cc48dcb19f7225a99625c30bca9b1442a0507c8
Timur <timatiblackstar666@gmail.com>
2018-05-04 16:36:42 +0300

    Writing a new comment to annotation @Configuration

--- Chapter2.1/src/main/java/com/gmail/timurworkspace/CDPlayerConfig.java
+++ Chapter2.1/src/main/java/com/gmail/timurworkspace/CDPlayerConfig.java
@@ -1,14 +1,18 @@
 package com.gmail.timurworkspace;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 
-@Configuration
-@ComponentScan(basePackageClasses = SgtPeppers.class) // Will be scan that package and any subpackages underneath it,
-// looking for classes that are annotated with @Component. It should find the
-// Compact-Disc class and automatically create a bean for it in Spring.
-
+@Configuration // annotation identifies this as a configuration class,
+// and it’s expected to contain details on beans that
+// are to be created in theSpring application context.
+@ComponentScan(basePackageClasses = SgtPeppers.class) // Will be scan that package
+// and any subpackages underneath it,looking for classes that are annotated with @Component.
+// It should find the Compact-Disc class and automatically create a bean for it in Spring.
 public class CDPlayerConfig {
+
+
 }
