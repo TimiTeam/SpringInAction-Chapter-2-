dc0425e5cf62ff72955eaff3a31c5992bc05854c
Timur <timatiblackstar666@gmail.com>
2018-05-06 01:46:25 +0300

    Add around advice method

--- AOPExample/src/main/java/concert/Audience.java
+++ AOPExample/src/main/java/concert/Audience.java
@@ -1,5 +1,6 @@
 package concert;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 @Aspect // This annotation indicates that Audience —it’s an aspect
@@ -8,24 +9,16 @@ public class Audience {
     @Pointcut("execution(** concert.Performance.perform(..))")
     public void performance(){}
 
-    @Before("performance()") // The advice method is called before the advised method is called.
-    public void silenceCellPhone(){
-        System.out.println("Silencing your cell phone");
-    }
-
-    @Before("performance()")
-    public void takingSeats(){
-        System.out.println("Taking seats");
-    }
-
-    @AfterReturning("performance()") //The advice method is called after the advised method returns.
-    public void applause(){
-        System.out.println("CLAP-CLAP-CLAP!!!");
-    }
-
-    @AfterThrowing("performance()") // The advice method is called after the advised method throws an exception.
-    public void demandingRefund(){
-        System.out.println("Demanding a refund");
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint jp) {
+        try {
+            System.out.println("Silencing cell phones");
+            System.out.println("Taking seats");
+            jp.proceed();
+            System.out.println("CLAP CLAP CLAP!!!");
+        } catch (Throwable e) {
+            System.out.println("Demanding a refund");
+        }
     }
 
 }
